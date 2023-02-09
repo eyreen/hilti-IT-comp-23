12ef263d4f8795750e9b7bfeb49e931c55470b0c
Arinn Danish bin Abdullah <64348931+eyreen@users.noreply.github.com>
2023-02-10 00:43:57 +0100

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const Equipment = require('./models/equipment');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/smart-construction-site', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(bodyParser.json());
+
+app.get('/equipment', (req, res) => {
+  Equipment.find({})
+    .then(equipment => {
+      res.json(equipment);
+    });
+});
+
+app.post('/equipment', (req, res) => {
+  const newEquipment = new Equipment({
+    name: req.body.name,
+    usage: req.body.usage,
+    maintenance: req.body.maintenance
+  });
+  newEquipment.save()
+    .then(equipment => {
+      res.json(equipment);
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Smart construction site server started');
+});
